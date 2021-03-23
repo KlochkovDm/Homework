3877b93829d092177f1ec4c71c35dbaa49303d67
Dmitry Klochkov <Klochkov.Dm595@yandex.ru>
2021-03-24 01:21:08 +0300

    Homework

---
+++ src/main/java/ru/geekbrains/lessons/lesson6/Bowl.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.lessons.lesson6;
+
+public class Bowl {
+
+    public Bowl() {
+    }
+
+    private int food;
+    public Bowl(int food) {
+        this.food = food;
+    }
+
+    public void putFood (int amount) {
+        this.food +=amount;
+        System.out.printf("We have put %d of food to the bowl. Now there is %d of food\n", amount, this.food);
+    }
+
+    public void takeFood(int amount) {
+        this.food -=amount;
+        System.out.printf("We have took %d of food from the bowl. Now there is %d of food\n", amount, this.food);
+    }
+
+
+    public void decreaseFood (int amount) {
+        if ((getFood() - amount) >= 0){
+            this.food -= amount;
+            System.out.println("Food decreased. There are " + getFood()+ " of food left in bowl");
+        }else System.out.println("Food is not enough to decrease. There are still " + getFood()+ " of food left in bowl");
+    }
+
+    public int getFood() {
+        return food;
+    }
+}
